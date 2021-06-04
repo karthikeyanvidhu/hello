0d64b1efa6839d765e4262dc401e5daee5a95963
karthikeyanvidhu <31299186+karthikeyanvidhu@users.noreply.github.com>
2021-06-04 11:44:00 +0530

    second

---
+++ src/main/java/com/hps/micro/HelloApplication.java
@@ -0,0 +1,33 @@
+package com.hps.micro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class HelloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloApplication.class, args);
+		}
+	
+	  @GetMapping({"/"})
+	  public String chkk()  { 
+		  return "Hello";
+	  }
+	  
+	  @GetMapping({"/hello"})
+	  public String chk()  { 
+		  return "Hello ,Application is UP";
+	  }
+	  
+	  @GetMapping({"/hello/world"})
+	  public String wlk()  { 
+		  return "Hello world ,Application is UP";
+	  }
+	  
+}
+
+
