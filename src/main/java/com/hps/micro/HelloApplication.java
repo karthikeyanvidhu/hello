89f9974e45485b12bef6006ebb23afe496d63e22
0020M6744 <0020M6744@LAPTOP-T4AVPS0K>
2021-06-07 20:34:32 +0530

    1.2 version

--- src/main/java/com/hps/micro/HelloApplication.java
+++ src/main/java/com/hps/micro/HelloApplication.java
@@ -15,7 +15,7 @@ public class HelloApplication {
 	
 	  @GetMapping({"/"})
 	  public String chkk()  { 
-		  return "Hello World.Version 1.1";
+		  return "Hello World.Version 1.2";
 	  }
 	  
 	  @GetMapping({"/hello"})
